2754615c36a0c1135f64fa385ba8416b67e63fb9
Kanish <kanishaka.sharma@gmail.comgit config --global user.email>
2022-03-23 15:28:50 -0400

    add: week 10 wednesday class

---
+++ lectures/week10/list_comprehension.py
@@ -0,0 +1,20 @@
+from typing import List, Optional, Union
+
+def sum_nested(obj: Union[int, List]) -> int:
+    """Return the sum of the numbers in <obj>
+    
+    Return 0 if there are no numbers
+    """
+    if isinstance(obj, int):
+        return int
+    else:
+        return sum([sum_nested(elem)] for elem in obj)
+    
+
+def flatten(obj) -> List[int]:
+    """Return a (non-nested) list of the integers in <obj>."""
+    if isinstance(obj, int):
+        return obj
+    else:
+        s = []
+        return sum(([flatten(elem)] for elem in obj), s) 
